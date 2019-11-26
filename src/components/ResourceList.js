8de2c1c483ce0a0f47887bb5cdfa40823d696c8d
Qasim Laghari <go.jets.laghari@gmail.com>
2019-11-26 14:32:32 -0800

    Update ResourceList component

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+    state = { resources: [] };
+
+    async componentDidMount() {
+       const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+       this.setState({ resources: response.data});
+    }
+
     render() {
         return (
         <div>
-            Resource List: {this.props.resource}
+            {this.state.resources.length}
         </div>
         );
     }
